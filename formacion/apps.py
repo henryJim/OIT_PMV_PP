7696fb933862f880183aec10d5e966a24f08eb46
Andres Sanabria <andrelipe897@gmail.com>
2024-12-16 14:46:47 -0500

    Eliminar archivos compilados (pyc) del control de versiones

--- formacion/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FormacionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'formacion'
--- gestion_instructores/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def gestion_instructor(request):
-    return render(request, 'gestion_instructor.html')
