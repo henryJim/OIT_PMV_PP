c183a853f727ed4a4af146dc96a80cf30d03b320
Alejandro Rivera <jalejandro142@gmail.com>
2025-01-08 01:42:33 -0500

    Se agrego script para despliegue de informacion pero falta por pulir o mejorar script

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
         }
     });
 });
+// <<<<Desplegable de informacion para el modulo instructores>>>
 
 $(document).ready(function () {
     // Inicializa DataTables
@@ -79,6 +80,68 @@ $(document).ready(function () {
 });
 
 
+
+// <<<<Desplegable de informacion para el modulo aprendices >>>
+
+$(document).ready(function () {
+    // Inicializa DataTables
+    var table = $('#aprendices').DataTable({
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/2.1.8/i18n/es-ES.json',
+        },
+    });
+
+    // Define la función para mostrar información adicional
+    function format(tr) {
+        var registroId = tr.data('id');
+        console.log(registroId)
+        var contenido = '<p>Cargando...</p>'; // Placeholder mientras se obtiene la información
+
+        // Llama a la API Django para obtener los detalles
+        $.ajax({
+            url: `/obtener_detalles_aprendiz/${registroId}/`,
+            method: 'GET',
+            async: false, // Debe ser síncrono para actualizar inmediatamente
+            success: function (data) {
+                contenido = `
+                    <div>
+                    <p><strong>Nombre:</strong> ${data.info_adicional.aprendiz.perfil.nom}</p>
+                    <p><strong>Apellido:</strong> ${data.info_adicional.aprendiz.perfil.apelli}</p>
+                    <p><strong>telefono:</strong> ${data.info_adicional.aprendiz.perfil.tele}</p>
+                    </div>
+                    `;
+            },
+            error: function () {
+                contenido = '<p>Error al cargar los detalles.</p>';
+            },
+        });
+
+        return contenido;
+    }
+
+    // Listener para expandir/cerrar filas
+    $('#aprendices tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr'); // Encuentra la fila
+        var row = table.row(tr); // Encuentra la fila en DataTables
+        var icon = $(this).find('i');
+
+        if (row.child.isShown()) {
+            // Si ya está expandido, ciérralo
+            row.child.hide();
+            tr.removeClass('shown');
+            icon.removeClass('bi-chevron-down').addClass('bi-chevron-right');
+
+        } else {
+            // Si está colapsado, expándelo
+            row.child(format(tr)).show();
+            tr.addClass('shown');
+            icon.removeClass('bi-chevron-right').addClass('bi-chevron-down');
+
+        }
+    });
+});
+
+
 /// <<<<Desplegable de informacion para el modulo administradores>>>
 
 $(document).ready(function () {
@@ -103,13 +166,13 @@ $(document).ready(function () {
             success: function (data) {
                 contenido = `
                     <div>
-                     <p><strong>Nombre:</strong> ${data.info_adicional.admin.area}</p>
-                    <p><strong>Nombre:</strong> ${data.info_adicional.perfil.nom}</p>
-                    <p><strong>Apellido:</strong> ${data.info_adicional.perfil.apelli}</p>
-                    <p><strong>Direccion:</strong> ${data.info_adicional.perfil.dire}</p>
-                    <p><strong>Telefono:</strong> ${data.info_adicional.perfil.tele}</p>
-                    <p><strong>Usuario:</strong> ${data.info_adicional.perfil.user.username}</p>
-                    <p><strong>Email Usuario:</strong> ${data.info_adicional.perfil.user.email}</p>
+                     <p><strong>Area:</strong> ${data.info_adicional.admin.area}</p>
+                    <p><strong>Nombre:</strong> ${data.info_adicional.admin.perfil.nom}</p>
+                    <p><strong>Apellido:</strong> ${data.info_adicional.admin.perfil.apelli}</p>
+                    <p><strong>Tipo de DNI:</strong> ${data.info_adicional.admin.perfil.tipo_dni}</p>
+                    <p><strong>Telefono:</strong> ${data.info_adicional.admin.perfil.tele}</p>
+                    <p><strong>Usuario:</strong> ${data.info_adicional.admin.perfil.user.username}</p>
+                    <p><strong>Email Usuario:</strong> ${data.info_adicional.admin.perfil.user.email}</p>
                     </div>
                     `;
             },
@@ -142,5 +205,69 @@ $(document).ready(function () {
         }
     });
 });
+
+
+/// <<<<Desplegable de informacion para el modulo lideres>>>
+
+$(document).ready(function () {
+    // Inicializa DataTables
+    var table = $('#lideres').DataTable({
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/2.1.8/i18n/es-ES.json',
+        },
+    });
+
+    // Define la función para mostrar información adicional
+    function format(tr) {
+        var registroId = tr.data('id');
+        console.log(registroId)
+        var contenido = '<p>Cargando...</p>'; // Placeholder mientras se obtiene la información
+
+        // Llama a la API Django para obtener los detalles
+        $.ajax({
+            url: `/detalle_lider/${registroId}/`,
+            method: 'GET',
+            async: false, // Debe ser síncrono para actualizar inmediatamente
+            success: function (data) {
+                contenido = `
+                    <div>
+                    <p><strong>Nombre:</strong> ${data.info_adicional.lider.perfil.nom}</p>
+                    <p><strong>Apellido:</strong> ${data.info_adicional.lider.perfil.apelli}</p>
+                    <p><strong>Telefono:</strong> ${data.info_adicional.lider.perfil.tele}</p>
+                    <p><strong>Usuario:</strong> ${data.info_adicional.lider.perfil.user.username}</p>
+                    <p><strong>Email Usuario:</strong> ${data.info_adicional.lider.perfil.user.email}</p>
+                    </div>
+                    `;
+            },
+            error: function () {
+                contenido = '<p>Error al cargar los detalles.</p>';
+            },
+        });
+
+        return contenido;
+    }
+
+    // Listener para expandir/cerrar filas
+    $('#lideres tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr'); // Encuentra la fila
+        var row = table.row(tr); // Encuentra la fila en DataTables
+        var icon = $(this).find('i');
+
+        if (row.child.isShown()) {
+            // Si ya está expandido, ciérralo
+            row.child.hide();
+            tr.removeClass('shown');
+            icon.removeClass('bi-chevron-down').addClass('bi-chevron-right');
+
+        } else {
+            // Si está colapsado, expándelo
+            row.child(format(tr)).show();
+            tr.addClass('shown');
+            icon.removeClass('bi-chevron-right').addClass('bi-chevron-down');
+
+        }
+    });
+});
+
 
 
